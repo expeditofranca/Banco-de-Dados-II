c65302d3f7cbc279619b074b63e84ce3f6c535e9
Expedito <expedito.luiz@escolar.ifrn.edu.br>
2026-05-05 14:30:01 -0300

    código de criação das tabelas projeto e atividade

--- AtividadeBD/init.sql
+++ AtividadeBD/init.sql
@@ -8,3 +8,20 @@ CREATE DATABASE atividade_db
 CREATE USER atividade_user WITH PASSWORD 'atividade123';
 
 GRANT ALL PRIVILEGES ON DATABASE atividade_db TO atividade_user;
+
+\c atividade_db;
+
+CREATE TABLE projeto (
+    id SERIAL PRIMARY KEY,
+    nome VARCHAR(100),
+    lider VARCHAR(100)
+);
+
+-- cria tabela de atividades
+CREATE TABLE atividade (
+    id SERIAL PRIMARY KEY,
+    titulo VARCHAR(100),
+    descricao TEXT,
+    projeto_id INT,
+    FOREIGN KEY (projeto_id) REFERENCES projeto(id)
+);
