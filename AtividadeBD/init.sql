ff5bca20e6cbc9fc64afaf8cd0fc6f684a789e99
Expedito <expedito.luiz@escolar.ifrn.edu.br>
2026-05-06 00:58:05 -0300

    atualizando init.sql

--- AtividadeBD/init.sql
+++ AtividadeBD/init.sql
@@ -1,15 +1,4 @@
-CREATE DATABASE atividade_db
-    WITH
-    OWNER = postgres
-    ENCODING = 'UTF8'
-    CONNECTION LIMIT = -1
-    IS_TEMPLATE = False;
-
-CREATE USER atividade_user WITH PASSWORD 'atividade123';
-
-GRANT ALL PRIVILEGES ON DATABASE atividade_db TO atividade_user;
-
-\c atividade_db;
+\c atividade_bd;
 
 CREATE TABLE projeto (
     id SERIAL PRIMARY KEY,
